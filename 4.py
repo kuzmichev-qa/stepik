3011834f7d16d6e07b6b2540bb70ba2cb5b8ab00
kuzmichev-qa <60174791+kuzmichev-qa@users.noreply.github.com>
2020-07-03 15:37:39 +0300

    tst cmt

--- 4.py
+++ 4.py
@@ -5,15 +5,3 @@ import math
 def calc(x):
     return str(math.log(abs(12*math.sin(int(x)))))
 
-
-driver = webdriver.Chrome()
-driver.get("http://suninjuly.github.io/get_attribute.html")
-
-x_element = driver.find_element_by_xpath("//img[@id = 'treasure']")
-x = x_element.get_attribute('valuex')
-y = calc(x)
-print(y)
-driver.find_element_by_xpath("//*[@id = 'answer']").send_keys(y)
-driver.find_element_by_xpath("//*[@id = 'robotCheckbox']").click()
-driver.find_element_by_xpath("//*[@id = 'robotsRule']").click()
-driver.find_element_by_xpath("//button[@class = 'btn btn-default']").click()
